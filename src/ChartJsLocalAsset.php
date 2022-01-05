05d168084fa976b669749c8bf531b98c6bb85270
Albert Borsos <albertborsos@me.com>
2022-01-05 19:15:43 +0100

    update chartjs dependency to ^3.0

--- src/ChartJsLocalAsset.php
+++ src/ChartJsLocalAsset.php
@@ -15,12 +15,12 @@ use yii\web\AssetBundle;
  *
  * ChartPluginAsset
  */
-class ChartJsAsset extends AssetBundle
+class ChartJsLocalAsset extends AssetBundle
 {
-    public $sourcePath = null;
+    public $sourcePath = '@npm/chart.js/dist';
 
     public $js = [
-        'https://cdn.jsdelivr.net/npm/chart.js@3.7.0/dist/chart.min.js'
+        'chart.min.js'
     ];
 
     public $depends = [
